4ebfdc43552c5d2afe62df9552581057f110221f
RainbowMan1 <nikeshsubedi2@gmail.com>
2020-06-25 11:47:59 -0400

    Added refresh

--- Flix/ViewControllers/MoviesViewController.m
+++ Flix/ViewControllers/MoviesViewController.m
@@ -14,15 +14,12 @@
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (strong, nonatomic) NSArray *movies;
+@property (strong, nonatomic) UIRefreshControl *refreshControl;
 @end
 
 @implementation MoviesViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    self.tableView.dataSource = self;
-    self.tableView.delegate = self;
-    // Do any additional setup after loading the view.
+-(void)fetchMovies {
     NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
     NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
@@ -34,14 +31,27 @@
                NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
                self.movies = dataDictionary[@"results"];
                [self.tableView reloadData];
+           }
+        [self.refreshControl endRefreshing];
+        }];
+    [task resume];
+}
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    // Do any additional setup after loading the view.
+    [self fetchMovies];
+    self.refreshControl =[[UIRefreshControl alloc] init];
+    [self.refreshControl addTarget:self action:@selector(fetchMovies) forControlEvents:UIControlEventValueChanged];
+    [self.tableView insertSubview:self.refreshControl atIndex:0];
                // TODO: Get the array of movies
                // TODO: Store the movies in a property to use elsewhere
                // TODO: Reload your table view data
-           }
-       }];
-    [task resume];
-    
 }
+       
+    
+
 
 /*
 #pragma mark - Navigation
@@ -59,7 +69,6 @@
     NSDictionary *movie = self.movies[indexPath.row];
     NSString *posterURLString = movie[@"poster_path"];
     NSString *fullPosterURLString = [baseURLString stringByAppendingString:posterURLString];
-    NSLog(@"%@", fullPosterURLString);
     NSURL *posterURL = [NSURL URLWithString:fullPosterURLString];
     cell.textLabel.text = movie[@"title"];
     cell.posterView.image = nil;
